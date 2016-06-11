c6114b70082cdff29ddb8c7606e89e1986757b68
warlordjones <warlordjones@gmail.com>
2016-06-11 20:18:12 +0100

    Allow altar crafting to be detected by the BQ standard crafting task.
    In a sort of wierd and hackyish way but nevermind that...

---
+++ src/main/java/xyz/brassgoggledcoders/bloodyquests/BloodyQuests.java
@@ -0,0 +1,69 @@
+package xyz.brassgoggledcoders.bloodyquests;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import WayofTime.bloodmagic.tile.TileAltar;
+import betterquesting.quests.QuestDatabase;
+import betterquesting.quests.QuestInstance;
+import betterquesting.quests.tasks.TaskBase;
+import betterquesting.quests.tasks.advanced.AdvancedTaskBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid=BloodyQuests.MODID, name="Bloody Quests", version="@VERSION@", dependencies="required-after:BloodMagic; required-after:betterquesting")
+public class BloodyQuests {
+	
+	public static final String MODID = "bloodyquests";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	//Semi-dirty hax
+	@SubscribeEvent
+	public void containerOpened(PlayerInteractEvent.RightClickBlock event)
+	{
+		EntityPlayer player = event.getEntityPlayer();
+		TileEntity te = event.getWorld().getTileEntity(event.getPos());
+		
+		if(player == null || event.getWorld().isRemote || te == null)
+			return;
+		
+		if(te instanceof TileAltar) {
+			TileAltar altar = (TileAltar) te;
+
+			for(Entry<AdvancedTaskBase,QuestInstance> set : GetAdvancedTasks(player.getUniqueID()).entrySet())
+			{
+				set.getKey().onItemCrafted(set.getValue(), player, altar.getStackInSlot(0));
+			}
+		}
+	}
+	
+	HashMap<AdvancedTaskBase, QuestInstance> GetAdvancedTasks(UUID uuid)
+	{
+		HashMap<AdvancedTaskBase, QuestInstance> map = new HashMap<AdvancedTaskBase, QuestInstance>();
+		
+		for(QuestInstance quest : QuestDatabase.getActiveQuests(uuid))
+		{
+			for(TaskBase task : quest.tasks)
+			{
+				if(task instanceof AdvancedTaskBase && !task.isComplete(uuid))
+				{
+					map.put((AdvancedTaskBase)task, quest);
+				}
+			}
+		}
+		
+		return map;
+	}
+}
