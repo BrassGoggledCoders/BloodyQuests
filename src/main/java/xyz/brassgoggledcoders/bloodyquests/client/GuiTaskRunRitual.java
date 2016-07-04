012c93aa6144d45c1d5a408146e32db68aade6a8
warlordjones <warlordjones@gmail.com>
2016-07-04 17:40:41 +0100

    Update for forge

--- src/main/java/xyz/brassgoggledcoders/bloodyquests/client/GuiTaskRunRitual.java
+++ src/main/java/xyz/brassgoggledcoders/bloodyquests/client/GuiTaskRunRitual.java
@@ -3,6 +3,7 @@ package xyz.brassgoggledcoders.bloodyquests.client;
 import betterquesting.client.gui.GuiQuesting;
 import betterquesting.client.gui.misc.GuiEmbedded;
 import betterquesting.client.themes.ThemeRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import xyz.brassgoggledcoders.bloodyquests.TaskRunRitual;
 
@@ -16,12 +17,15 @@ public class GuiTaskRunRitual extends GuiEmbedded {
 
 	@Override
 	public void drawGui(int mx, int my, float partialTick) {
-		screen.mc.fontRenderer.drawString(I18n.format("bloodyquests.task.runritual", new Object[0]),
+		Minecraft.getMinecraft().fontRendererObj.drawString(I18n.format("bloodyquests.task.runritual", new Object[0]),
 				posX + sizeX / 2
-						- screen.mc.fontRenderer.getStringWidth(I18n.format("bloodyquests.task.runritual", "")) / 2,
+						- Minecraft.getMinecraft().fontRendererObj
+								.getStringWidth(I18n.format("bloodyquests.task.runritual", "")) / 2,
 				posY, ThemeRegistry.curTheme().textColor().getRGB());
-		screen.mc.fontRenderer.drawString("Ritual: " + targetRitualName, posX + sizeX / 2
-				- screen.mc.fontRenderer.getStringWidth("Ritual: " + I18n.format(targetRitualName, new Object[0])) / 2,
+		Minecraft.getMinecraft().fontRendererObj.drawString("Ritual: " + targetRitualName,
+				posX + sizeX / 2
+						- Minecraft.getMinecraft().fontRendererObj
+								.getStringWidth("Ritual: " + I18n.format(targetRitualName, new Object[0])) / 2,
 				posY + 20, ThemeRegistry.curTheme().textColor().getRGB());
 	}
 
