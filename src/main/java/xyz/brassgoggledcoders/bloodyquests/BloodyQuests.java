68e37baa74b1bbe8902e5794af410d0f930ead9e
warlordjones <warlordjones@gmail.com>
2017-07-05 22:18:50 +0100

    Use correct casing in BM dependency

--- src/main/java/xyz/brassgoggledcoders/bloodyquests/BloodyQuests.java
+++ src/main/java/xyz/brassgoggledcoders/bloodyquests/BloodyQuests.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = BloodyQuests.MODID, name = "Bloody Quests", version = "@VERSION@",
-		dependencies = "required-after:BloodMagic; required-after:bq_standard")
+		dependencies = "required-after:bloodmagic; required-after:bq_standard")
 public class BloodyQuests {
 
 	public static final String MODID = "bloodyquests";
